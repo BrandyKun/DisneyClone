fce0dba859eda7c634b563c17cf0762090d1af54
BrandyKun <55144997+BrandyKun@users.noreply.github.com>
2021-04-22 18:20:13 +0100

    Login and nav bar

---
+++ src/components/Header.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+const Header = (props) => {
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/disneylogo.svg" alt="Disney Logo" />
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+            <img src="/images/home-icon.svg" alt="Home"/>
+        </a>
+        <span>Home</span>
+      </NavMenu>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  marging-top: 4px;
+  font-size: 0;
+  display: inline-block;
+
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;    
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0 auto 0 25px;
+    padding: 0px;
+    position; relative;
+
+    @media(max-width: 768px){
+        display:none;
+    }
+`;
+
+export default Header;
